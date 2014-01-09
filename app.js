9f439459eaf3d563f91756766a6526f9204efbf1
daipeng <daipeng123456@gmail.com>
2014-01-09 13:24:14 +0800

    fix

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
+var MongoStore = require('connect-mongo');
+var settings = require('../settings');
+
 var app = express();
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -20,7 +24,12 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
-app.use(express.session());
+app.use(express.session({
+  secret: settings.cookieSecret,
+  store: new MongoStore({
+      db: settings.db
+  })
+}));
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -30,8 +39,37 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/hello', routes.hello); 
-app.get('/users', user.list);
+app.get('/u/:user', routes.user);
+app.post('/post', routes.post);
+app.get('/reg', routes.reg);
+app.post('/reg', routes.doReg);
+app.get('/login', routes.login);
+app.post('/login', routes.doLogin);
+app.get('/logout', routes.logout);
+
+
+// 动态视图
+app.dynamicHelper({
+    user: function(req, res){
+        return req.session.user;
+    },
+    error: function(req, res){
+        var err = req.flash('error');
+        if(err.length){
+            return err;
+        }else{
+            return null;
+        }
+    },
+    success: function(req, res){
+        var succ = req.flash('success');
+        if(succ.length){
+            return succ;
+        }else{
+            return null;
+        }
+    },
+})
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,69 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '首页' });
 };
 
-exports.hello = function(req, res){
-    res.render('hello', {
-        title: 'express',
-        layout: 'layout',
+exports.user = function(req, res){
+    
+};
+
+exports.post = function(req, res){
+    
+};
+
+exports.reg = function(req, res){
+    res.render('reg', {
+        title: '用户注册',
     });
+};
+
+exports.doReg = function(req, res){
+    // 检测用户口令是否一致
+    if(req.body['password-repeat'] != req.body['password']){
+        req.flash('error', '再次输入的口令不一致');
+        return res.redirect('/reg');
+    }
+
+    // 生成口令的散列值
+    var md5 = crypto.creatHash('md5');
+    var password = md5.update(req.body.password).digest('base64');
+
+    var newUser = new User({
+        name: req.body.username,
+        password: password,
+    });
+
+    // 检查用户是否以存在
+    User.get(newUser.name, function(err, User){
+        if(user){
+            err = "Username already exists";
+        }
+        if(err){
+            req.flash('error', err);
+            return res.redirect('/reg');
+        }
+        // 如果不存在则新建用户
+        newUser.save(function(err){
+            if(err){
+                req.flash('error', err);
+                return res.redirect('/reg');
+            }
+            req.session.user = newUser;
+            req.flash('success', '注册成功');
+            res.redirect('/');
+        });
+    });
+};
+
+exports.login = function(req, res){
+    
+};
+
+exports.doLogin = function(req, res){
+    
+};
+
+exports.logout = function(req, res){
+    
 };
